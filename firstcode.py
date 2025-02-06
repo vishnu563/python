4fe3ab4cd03d919e67ea5bb4846e0db3078a93d0
vishnu563 <vishnuprasanth050603@gmail.com>
2025-02-06 10:04:15 +0530

    Learning...

--- firstcode.py
+++ firstcode.py
@@ -1,5 +1,12 @@
-name =str(input("Enter Your Name "))
-age=int(input("Enter Your Age "))
+# name =str(input("Enter Your Name "))
+# age=int(input("Enter Your Age "))
 
-print("Your name is :",name)
-print("Your age is :",age)
+# print("Your name is :",name)
+# print("Your age is :",age)
+
+# ifelse
+TharunikaAnswer = input("Subash ask to tharunika can we marry?\nTharunika said :")
+if(TharunikaAnswer == "okay"):
+    print("Subash Weds Tharunika")
+else:
+    print("Subash weds Someone")
