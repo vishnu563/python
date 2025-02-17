978d78e63f29e4351cb92851a85ac92654d26b40
vishnu563 <vishnuprasanth050603@gmail.com>
2025-02-17 08:13:04 +0530

    If else

--- firstcode.py
+++ firstcode.py
@@ -5,8 +5,22 @@
 # print("Your age is :",age)
 
 # ifelse
-TharunikaAnswer = input("Subash ask to tharunika can we marry?\nTharunika said :")
-if(TharunikaAnswer == "okay"):
-    print("Subash Weds Tharunika")
+
+
+# TharunikaAnswer = input("Subash ask to tharunika can we marry?\nTharunika said :")
+# if(TharunikaAnswer == "okay"):
+#     print("Subash Weds Tharunika")
+# else:
+#     print("Subash weds Someone")
+
+# mark = int(input("Enter your Mark : "))
+# if(mark>34):
+#     print("You are pass")
+# else:
+#     print("You are fail")
+
+income = int(input("Enter Your Income : "))
+if(income>7000):
+    print("Not Eligible for Scholarship")
 else:
-    print("Subash weds Someone")
+    print("Eligible for Scholarship")
