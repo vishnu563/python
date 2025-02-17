6d69013a502606a2b89eb20702d53fbfe52fabd7
vishnu563 <vishnuprasanth050603@gmail.com>
2025-02-17 12:31:00 +0530

    For

--- firstcode.py
+++ firstcode.py
@@ -19,8 +19,35 @@
 # else:
 #     print("You are fail")
 
-income = int(input("Enter Your Income : "))
-if(income>7000):
-    print("Not Eligible for Scholarship")
-else:
-    print("Eligible for Scholarship")
+# Number = int(input("Enter Number"))
+# if(Number%2==0):
+#     print("Number is Even")
+# else:
+#     print("Number is Odd")
+
+# ScorePercentage = int(input("Enter Percentage "))
+# if(ScorePercentage>=70):
+#     name=input("Enter Name: ")
+#     department=input("Enter Department: ")
+#     age=int(input("enter age: "))
+#     print(name,department,age, "You are Eligible")
+# else:
+#     print("You are not Eligible")
+
+# sub1=int(input("Enter mark of sub1: "))
+# sub2=int(input("Enter mark of sub2: "))
+# sub3=int(input("Enter mark of sub3: "))
+# sub4=int(input("Enter mark of sub4: "))
+# sub5=int(input("Enter mark of sub5: "))
+
+# totalMark = sub1+sub2+sub3+sub4+sub5
+# print("Total Mark is: ",totalMark)
+# avg=(totalMark/500)*100
+# print("Average is: ",avg)
+# if(avg>35):
+#     print("you are good to go")
+# else:
+#     print("Additional class required")
+
+for i in range(5):
+    print(i)
